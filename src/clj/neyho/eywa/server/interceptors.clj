23dd0f9ecbe94bdc9e4164b5e3fc91f5f5d22419
Robert Geršak <robi@neyho.com>
2026-07-31 11:22:48 +0200

    Fix for SPA interceptor

--- src/clj/neyho/eywa/server/interceptors.clj
+++ src/clj/neyho/eywa/server/interceptors.clj
@@ -102,70 +102,71 @@
                (recur (butlast sections))))))))})
 
 
+(defn- find-nearest-index-html
+  "Walk up path sections looking for nearest index.html.
+   Returns the file path if found, nil otherwise."
+  [root sections]
+  (loop [sections sections]
+    (if (empty? sections)
+      ;; Check root index.html
+      (let [target (str root "/index.html")]
+        (when (fs/exists? target) target))
+      ;; Check current path + index.html
+      (let [target (str root "/" (str/join "/" sections) "/index.html")]
+        (if (fs/exists? target)
+          target
+          (recur (butlast sections)))))))
+
 (defn make-spa-interceptor
+  "SPA interceptor that serves static files and falls back to nearest index.html.
+
+   IMPORTANT: All logic is in :enter phase because Pedestal's not-found interceptor
+   has a :leave handler that runs BEFORE this interceptor's :leave (due to reverse
+   order), which would set 404 before we can serve index.html."
   ([root]
    (interceptor
-     {:name ::spa-dynamic 
-      :enter
-      (fn [{{:keys [path-info uri] :as request} :request
-            :as context}]
-        (if-not root context
-          (let [extension (re-find #"(?<=\.).*?$" uri)] 
-            ;; If extension exists
-            (if (some? extension)
-              ;; Try to return that file
-              (as-> (or path-info uri) path
-                (if (.startsWith path "/")
-                  (subs path 1)
-                  path)
-                (let [target (str root "/" path)]
-                  (log/tracef "Returning resource file %s, for path %s" uri path-info)
-                  (if (fs/exists? target)
+    {:name ::spa-dynamic
+     :enter
+     (fn [{{:keys [path-info uri] :as request} :request
+           response :response
+           :as context}]
+       (if (or (not root) response)
+         context
+         (let [extension (re-find #"(?<=\.).*?$" uri)]
+           (if (some? extension)
+             ;; Has extension - try to return that file
+             (as-> (or path-info uri) path
+               (if (.startsWith path "/")
+                 (subs path 1)
+                 path)
+               (let [target (str root "/" path)]
+                 (log/tracef "Returning resource file %s, for path %s" uri path-info)
+                 (if (fs/exists? target)
+                   (chain/terminate
                     (assoc context :response
                            (-> (response/file-response target)
-                               (head/head-response request)))
-                    (chain/terminate
-                      (assoc context
-                             :response {:status 404
-                                        :body "Not found!"})))))
-              ;; Otherwise proceed
-              context))))
-      ;; Leave will only happen when requested URI ends without extension
-      ;; That is when static file isn't directly required
-      :leave
-      (fn [{{:keys [path-info uri] :as request} :request
-            response :response
-            :as context}]
-        (if response
-          ;; If there is some kind of response
-          context
-          ;; Otherwise return root html
-          (if-not root
-            context 
-            (loop [sections (remove empty? (str/split (or path-info uri) #"/"))]
-              (if (empty? sections)
-                ;; If there are no more sections
-                (let [target (fs/file root "/index.html")]
-                  (if (fs/exists? target)
-                    (assoc context :response
-                           (-> (response/file-response target)
-                               (assoc-in [:headers "Content-Type"] "text/html")
-                               (head/head-response request)))
-                    (chain/terminate
-                      (assoc context
-                             :response {:status 404
-                                        :body "Not found!"}))))
-                ;;
-                (let [target (str
-                               root "/"
-                               (str/join "/" sections)
-                               "/index.html")]
-                  (if (fs/exists? target)
-                    (assoc context :response
-                           (-> (response/file-response target)
-                               (assoc-in [:headers "Content-Type"] "text/html")
-                               (head/head-response request)))
-                    (recur (butlast sections)))))))))})))
+                               (head/head-response request))))
+                   context)))
+             ;; No extension - SPA route, find nearest index.html
+             (let [sections (remove empty? (str/split (or path-info uri) #"/"))]
+               (if-some [target (find-nearest-index-html root sections)]
+                 (chain/terminate
+                  (assoc context :response
+                         (-> (response/file-response target)
+                             (assoc-in [:headers "Content-Type"] "text/html")
+                             (head/head-response request))))
+                 context))))))})))
+
+(defn make-info-interceptor
+  [info]
+  (interceptor
+   {:name ::info
+    :enter (fn [ctx]
+             (chain/terminate
+              (assoc ctx :response {:status 200
+                                    :body info})))}))
+
+
 
 
 (comment
